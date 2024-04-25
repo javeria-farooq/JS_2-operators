46f2fcc4ef7150355d8ad108015ef5ea702a116e
javeria-farooq <gettingeducation6@gmail.com>
2024-04-25 16:38:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+/*ab hmare ps aata h exponentiation ye powers lgane k lye use hta h jb hmn ksi ek variable k
+upr koi dsra variable as a power lgana h tb hm ** double asterisk ka use krte hn jsse pta
+chlta h k yhn exponent wla kaam hga. ye basically kaam ks trhn krta h ye kaam ese krta h
+double asterisk k baad jo hta h ye oose power bna dtta h. yni k yhn ** k bd b th js wjh se
+inho ne exponent jo h wo b ko bna dia or jb b ko power bnaya t zhir h b ki value ko hi
+power bnaega na.*/
+
+/*//////Exponentiation */
+
+/*let a = 5
+let b = 2
+console.log("a = ", a, "&", "b  = ", b)
+console.log("a ** b = ",a**b)*/
+
+/*///////unary operator:*/ // incr & decr*/
+
+/*let a = 5
+let b = 2
+console.log("a = ", a, "&", "b  = ", b)
+a = a+1;
+a += 1       
+a++
+console.log("a = ", a)
+
+b = a++
+console.log("a = ", a, " b = ", b) // a= 6, b=5
+
+let age = 20
+let age2 = age++
+document.write("ab mne post incr kia h lkn ye frq yd rkhna h k mre ps 2 variables hn ek wo jsmn incr kia h or wo jsmn incr store kia h t jsmn incr store kia h usmn mne incr store nh kia blke post incr store kia h js wjh se wo usmn incr krega hi nh or jsmn incr kia h jb oose ek step k baad cl krngi t usmn incr hwa wa hga ", age)*/
+
+/*////////////// Assignment operators:*/
+
+/*let a = 5
+let b = 2
+console.log("a = ", a, " & b = ", b)
+
+a += b // 7
+a -= b // 3
+a *= b //10
+a /= b // 2.5
+a %= b // 1
+a **= b // 25
+console.log("a = ", a)*/
+
+/*/////////////comaprison operators:*/
+
+/*let a = 5
+let b = 2
+console.log("a = ", a, " & b = ", b)
+
+console.log("a == b", a == b) // false
+console.log("a != b", a != b) // true
+console.log("a > b", a > b) // true
+console.log("5 >= 2", a >= b) // true
+console.log("5 < 2", a < b) // false
+console.log("5 <= 2", a <= b) // false
+console.log("5 <= 5", a<=a)
+console.log("2 >= 2", b >= b)
+
+let x = 5 
+let y = "5"
+console.log("x = ", x, " & y = ", y)
+console.log('x === y', x === y) // false
+console.log('x !== y', x !== y) // true*/
+
+/*//////// logical operators:*/
+
+/*let a = 6
+let b = 5
+console.log("a = ", a, " & b = ", b)
+
+console.log("a > b && a === 6", a > b && a === 6) // true //2non condition true hn
+console.log("a < b && a === 6", a < b && a === 6) // false
+console.log("a > b && a !== 6", a > b && a !== 6) // false
+console.log("a < b && a !== 6", a < b && a !== 6) // false
+
+console.log("a > b || a === 6", a > b || a === 6) //true
+console.log("a < b || a === 6", a < b || a === 6) //true
+console.log("a > b || a !== 6", a > b || a !== 6) // true
+console.log("a < b || a !== 6", a < b || a !== 6) // false*/
+
+
+/*////////// ternary operator */
+/*console.log("!(a === 6) && a > b = ", !(a === 6) && a > b) // false ek condition
+console.log("!(a === 6) && a < b = ", !(a === 6) && a < b) // false dnon condition
+console.log(`a === 2 && !(a < 4) = ${a === 2 && !(a < 4)}`)
+console.log("!(a === 5) && a > b = ", !(a === 5) && a > b) // true 2non condition*/
